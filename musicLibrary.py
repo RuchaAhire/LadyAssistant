00c8bb2a83cba03369c46eb4e90965e791632e56
RuchaAhire <ahirerucha81@gmail.com>
2024-07-19 20:08:59 +0530

    Add files via upload
    A Lady-like assistant is built into Python that listens to voice commands, answers questions, opens apps, plays music and provides new media. is specific to easily combine multiple components in a single tool

---
+++ musicLibrary.py
@@ -0,0 +1,8 @@
+music = {
+    "stealth": "https://www.youtube.com/watch?v=U47Tr9BB_wE",
+    "march": "https://www.youtube.com/watch?v=Xqeq4b5u_Xw",
+    "skyfall": "https://www.youtube.com/watch?v=DeumyOzKqgI&pp=ygUHc2t5ZmFsbA%3D%3D",
+    "wolf": "https://www.youtube.com/watch?v=ThCH0U6aJpU&list=PLnrGi_-oOR6wm0Vi-1OsiLiV5ePSPs9oF&index=21",
+    "rabta": "https://www.youtube.com/watch?v=zAU_rsoS5ok&list=RDzAU_rsoS5ok&start_radio=1",
+    "kokan": 'https://www.youtube.com/watch?v=6dmH5DfEn4o&pp=ygUFc29uZ3M%3D'
+}
